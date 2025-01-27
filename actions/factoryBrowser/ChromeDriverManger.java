7ac02f97abd9ad58520e405ba7be31c1649dc56a
thinhltpre <thinhlt@presight.io>
2025-01-27 19:35:43 +0700

    improve

---
+++ actions/factoryBrowser/ChromeDriverManger.java
@@ -0,0 +1,15 @@
+package factoryBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeDriverManger implements BrowserFactory{
+    @Override
+    public WebDriver getBrowserDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        return  new ChromeDriver(options);
+    }
+}
